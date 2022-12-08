c0084e4207ec81ab7dfb86c847d1901689e4b603
Arman <arman_m121@yahoo.com>
2022-12-08 21:50:58 +0330

    add image

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,5 +1,6 @@
 import splash from './splash.png'; 
 import pizza from './1.jpg'; 
 import shoppingCart from './shopping-cart.png'; 
+import headerFood from './header_food.jpg'; 
 
-export default { splash,pizza,shoppingCart}
+export default { splash,pizza,shoppingCart,headerFood}
