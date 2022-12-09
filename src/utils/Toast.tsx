7b8bc5cea2e184f73f9e8924e1d3d67d3b54f9ad
Arman <arman_m121@yahoo.com>
2022-12-09 14:49:05 +0330

    Add useShowSuccess toast

--- src/utils/Toast.tsx
+++ src/utils/Toast.tsx
@@ -2,9 +2,9 @@ import { useToast, Box, Text } from 'native-base'
 import { Colors } from '~/style';
 import { fontWeight } from '~/utils/Style';
 
-export const useShowMessage = () => {
+export const useShowError = () => {
     const toast = useToast()
-    const show = (title: string) => {
+    const showError = (title: string) => {
         toast.show({
             render: () => {
                 return <Box w='200' h='90' alignItems='center' justifyContent='center' bg={Colors.ERROR} px="2" py="1" rounded="lg" mb={5}>
@@ -13,5 +13,18 @@ export const useShowMessage = () => {
             }
         });
     }
-    return { show }
+    return { showError }
+}
+export const useShowSuccess = () => {
+    const toast = useToast()
+    const showSuccess = (title: string) => {
+        toast.show({
+            render: () => {
+                return <Box w='200' h='90' alignItems='center' justifyContent='center' bg={Colors.SUCCESS} px="2" py="1" rounded="lg" mb={5}>
+                    <Text color={Colors.PRIMARY_LIGHT} fontWeight={fontWeight.bold} fontSize='15'>{title}</Text>
+                </Box>;
+            }
+        });
+    }
+    return { showSuccess }
 }
