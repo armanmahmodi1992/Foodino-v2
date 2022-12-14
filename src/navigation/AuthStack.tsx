47d6f62af48b3a44e7347cf39e1228ad54b9eeee
Arman <arman_m121@yahoo.com>
2022-12-14 10:52:32 +0330

    add header title

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LoginScreen, UserScreen, RegisterScreen, SettingScreen } from '~/screens';
+import { LoginScreen, UserScreen, RegisterScreen, SettingScreen, ForgetPassword } from '~/screens';
+import { CustomHeader } from '~/component';
 
-export type AuthStackParamList = { LoginScreen: any; UserScreen: any; RegisterScreen: any; SettingScreen: any };
+export type AuthStackParamList = { LoginScreen: any; UserScreen: any; RegisterScreen: any; SettingScreen: any; ForgetPassword: any };
 const Stack = createNativeStackNavigator();
 
 export default function AuthStackNavigator() {
@@ -11,18 +12,67 @@ export default function AuthStackNavigator() {
             <Stack.Screen
                 name={'LoginScreen'}
                 component={LoginScreen}
+                options={
+                    {
+                        headerTitle: 'ورود کاربر',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
             />
             <Stack.Screen
                 name={'RegisterScreen'}
                 component={RegisterScreen}
+                options={
+                    {
+                        headerTitle: ' ثبت نام',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
             />
             <Stack.Screen
                 name={'UserScreen'}
                 component={UserScreen}
+                options={
+                    {
+                        headerTitle: ' صفحه کاربر ',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
             />
             <Stack.Screen
                 name={'SettingScreen'}
                 component={SettingScreen}
+                options={
+                    {
+                        headerTitle: ' تنظیمات',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
+            />
+            <Stack.Screen
+                name={'ForgetPassword'}
+                component={ForgetPassword}
+                options={
+                    {
+                        headerTitle: ' فراموشی رمز',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
             />
 
         </Stack.Navigator>
