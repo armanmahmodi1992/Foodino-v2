ae749c0723ca782c429ee9547446752615aba412
Arman <arman_m121@yahoo.com>
2022-12-14 10:20:35 +0330

    add Header into HomeScreen

--- src/navigation/HomeStack.tsx
+++ src/navigation/HomeStack.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen, FoodMenuScreen } from '~/screens';
+import { CustomHeader } from '~/component';
 export type HomeStackParamList = { HomeScreen: undefined; FoodMenuScreen: { subset: string } };
 export default function HomeStackNavigator() {
     const Stack = createNativeStackNavigator();
@@ -9,6 +10,15 @@ export default function HomeStackNavigator() {
             <Stack.Screen
                 name={'HomeScreen'}
                 component={HomeScreen}
+                options={
+                    {
+                        headerTitle: 'صفحه اصلی',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
             />
             <Stack.Screen
                 name={'FoodMenuScreen'}
