bde035f0ce21b01fb5dd04c50159dba28e64e000
Arman <arman_m121@yahoo.com>
2022-12-19 09:54:15 +0330

    add safe area & status bar

--- App.tsx
+++ App.tsx
@@ -1,23 +1,28 @@
-import { SafeAreaView } from 'react-native';
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import HomeStack from '~/navigation/MainStack';
-import { NativeBaseProvider } from 'native-base';
-import { navigationRef } from '~/navigation/Methods';
 import {
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query';
+import { NativeBaseProvider } from 'native-base';
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+import HomeStack from '~/navigation/MainStack';
+import { navigationRef } from '~/navigation/Methods';
+import { Colors } from '~/style';
 const queryClient = new QueryClient()
 
 export default function App() {
 
   return (
+
     <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" hidden={false} translucent={false} backgroundColor={Colors.PRIMARY_LIGHT} />
       <QueryClientProvider client={queryClient}>
         <NativeBaseProvider >
           <NavigationContainer ref={navigationRef}>
+
             <HomeStack />
+
           </NavigationContainer>
         </NativeBaseProvider>
       </QueryClientProvider>
