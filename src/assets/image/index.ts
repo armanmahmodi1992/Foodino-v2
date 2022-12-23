fe5b61d9b5f56833e7e8bacba913070b5da44119
Arman <arman_m121@yahoo.com>
2022-12-23 17:27:49 +0330

    add empty order image

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -2,5 +2,11 @@ import splash from './splash.png';
 import pizza from './1.jpg'; 
 import shoppingCart from './shopping-cart.png'; 
 import headerFood from './header_food.jpg'; 
+import emptyOrder from './empty-order.png'; 
 
-export default { splash,pizza,shoppingCart,headerFood}
+export default { 
+    splash,pizza,
+    shoppingCart,
+    headerFood,
+    emptyOrder
+}
