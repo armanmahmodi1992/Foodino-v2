a6a2e9ae81da701f46b57f21239d3d66f2a9de34
Arman <arman_m121@yahoo.com>
2022-12-26 10:16:50 +0330

    add no-internet image

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -3,10 +3,12 @@ import pizza from './1.jpg';
 import shoppingCart from './shopping-cart.png'; 
 import headerFood from './header_food.jpg'; 
 import emptyOrder from './empty-order.png'; 
+import noInternet from './no-internet.png'; 
 
 export default { 
     splash,pizza,
     shoppingCart,
     headerFood,
-    emptyOrder
+    emptyOrder,
+    noInternet
 }
