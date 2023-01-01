b7121ff0e771bbcf6f67d14d7f2f083a032dbaf4
Arman <arman_m121@yahoo.com>
2023-01-01 16:02:25 +0330

    remove image

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,14 +1,5 @@
-import splash from './splash.png'; 
-import pizza from './1.jpg'; 
-import shoppingCart from './shopping-cart.png'; 
-import headerFood from './header_food.jpg'; 
-import emptyOrder from './empty-order.png'; 
 import noInternet from './no-internet.png'; 
 
 export default { 
-    splash,pizza,
-    shoppingCart,
-    headerFood,
-    emptyOrder,
     noInternet
 }
