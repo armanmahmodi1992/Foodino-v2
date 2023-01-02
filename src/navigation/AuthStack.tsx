11b7a01945223529be7f297f63325d4ed39c1872
Arman <arman_m121@yahoo.com>
2023-01-02 19:23:26 +0330

    add new screen into AuthStack

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LoginScreen, UserScreen, RegisterScreen, SettingScreen, ForgetPassword } from '~/screens';
 import { CustomHeader } from '~/component';
 import authStore from '~/store/AuthStore';
+import {
+    LoginScreen,
+    UserScreen, RegisterScreen,
+    SettingScreen,
+    ForgetPassword,
+    EditScreen,
+    AboutApplicationScreen,
+    PaymentMethodsScreen,
+    DeliveryScreen,
+    SupportScreen
+} from '~/screens';
 
-export type AuthStackParamList = { LoginScreen: any; UserScreen: any; RegisterScreen: any; SettingScreen: any; ForgetPassword: any };
+export type AuthStackParamList = {
+    LoginScreen: any;
+    UserScreen: any;
+    RegisterScreen: any;
+    SettingScreen: any;
+    ForgetPassword: any;
+    EditScreen: any;
+    AboutApplicationScreen: any;
+    PaymentMethodsScreen: any;
+    DeliveryScreen: any;
+    SupportScreen: any
+};
 
 const Stack = createNativeStackNavigator();
 
@@ -80,7 +101,71 @@ export default function AuthStackNavigator() {
                     }
                 }
             />
-
+            <Stack.Screen
+                name={'EditScreen'}
+                component={EditScreen}
+                options={
+                    {
+                        headerTitle: 'ویرایش اطلاعات',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
+            />
+            <Stack.Screen
+                name={'AboutApplicationScreen'}
+                component={AboutApplicationScreen}
+                options={
+                    {
+                        headerTitle: 'درباره فودینو',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
+            />
+            <Stack.Screen
+                name={'PaymentMethodsScreen'}
+                component={PaymentMethodsScreen}
+                options={
+                    {
+                        headerTitle: 'روشهای پرداخت',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
+            />
+            <Stack.Screen
+                name={'DeliveryScreen'}
+                component={DeliveryScreen}
+                options={
+                    {
+                        headerTitle: 'روشهای ارسال',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
+            />
+            <Stack.Screen
+                name={'SupportScreen'}
+                component={SupportScreen}
+                options={
+                    {
+                        headerTitle: 'پشتیبانی',
+                        headerShown: true,
+                        header: ({ route, options, navigation }: any) => (
+                            <CustomHeader back {...{ route, options, navigation }} />
+                        ),
+                    }
+                }
+            />
         </Stack.Navigator>
     );
 }
