caaf507ba7536e15d3be85a1a3a2dcc95a77ed92
Arman <arman_m121@yahoo.com>
2023-01-08 19:58:38 +0330

    add mutation for delete

--- src/component/atom/CartCard.tsx
+++ src/component/atom/CartCard.tsx
@@ -2,7 +2,7 @@ import { HStack, Image, Text, View, VStack } from 'native-base';
 import React, { useEffect, useState } from 'react';
 import { Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { CustomButton, QuestionModal } from '~/component';
+import { CustomButton, CustomContainer, QuestionModal } from '~/component';
 import { cartMinus, cartPlus } from '~/component/atom/CardCounter';
 import { useUpdateFoodList, useDeleteCart } from '~/hooks';
 import { Colors } from '~/style';
@@ -17,7 +17,7 @@ export default function CartCard({ item }: { item: any }) {
 
     const id = item?.id
 
-    const { mutate } = useUpdateFoodList()
+    const { mutate: update, isLoading } = useUpdateFoodList()
     const { mutate: Delete } = useDeleteCart()
 
     const onLogOutPressHandler = () => {
@@ -30,8 +30,18 @@ export default function CartCard({ item }: { item: any }) {
 
     useEffect(() => {
         item.number = cart
-        mutate(item, {
+        update(item, {
             onSuccess: (data) => {
+                const item = data.data
+                if (item.number == 0) {
+                    Delete(id, {
+                        onSuccess: (data) => {
+                            console.log('status', data.status)
+                        },
+                        onError: (error) => {
+                        }
+                    })
+                }
             },
             onError: (error) => {
             }
@@ -51,37 +61,38 @@ export default function CartCard({ item }: { item: any }) {
     };
 
     return (
+        <CustomContainer isLoading={isLoading}>
+            <View style={styles.content}>
 
-        <View style={styles.content}>
+                <HStack h='160' w='95%' direction='row-reverse' borderWidth='1' bgColor={Colors.PRIMARY_LIGHT} borderColor={Colors.PRIMARY_LIGHT} alignItems='center' borderRadius='10' marginTop='2' p='3'>
+                    <Image source={{ uri: item?.pic }} style={styles.image} alt='image' />
+                    <VStack space='2' pr='1' flex={1} m='1' >
+                        <Text style={[styles.text, { height: 30 }]}>{item?.name}</Text>
+                        <Text style={[styles.text, { height: 30 }]}>{item?.price} ريال</Text>
+                    </VStack>
+                    <VStack space='3' paddingLeft='2'  >
+                        <TouchableOpacity onPress={() => onLogOutPressHandler()}>
+                            <Icon name='trash-o' size={30} color={Colors.ERROR} />
+                        </TouchableOpacity>
+                        <HStack width='100' justifyContent='space-between'>
+                            <CustomButton title='-' onPress={() => setCart(cartMinus(cart))} buttonStyle={{ width: 29, height: 35, backgroundColor: Colors.PRIMARY_LIGHT }} textStyle={{ fontSize: 20, color: Colors.SECONDARY_LIGHT }} />
+                            <Text style={styles.text}>{cart}</Text>
+                            <CustomButton title='+' onPress={() => setCart(cartPlus(cart))} buttonStyle={{ width: 29, height: 35, backgroundColor: Colors.PRIMARY_LIGHT }} textStyle={{ fontSize: 20, color: Colors.SECONDARY_LIGHT }} />
+                        </HStack>
+                    </VStack>
+                    <QuestionModal
+                        option1="انصراف"
+                        option2="حذف"
+                        visible={logoutModalVisible}
+                        onClose={onCloseLogoutModal}
+                        option1OnPress={onCloseLogoutModal}
+                        option2OnPress={deleteCart}
+                        title="آیا مطمئنید که میخواهید حذف کنید؟"
+                    />
+                </HStack>
 
-            <HStack h='160' w='95%' direction='row-reverse' borderWidth='1' bgColor={Colors.PRIMARY_LIGHT} borderColor={Colors.PRIMARY_LIGHT} alignItems='center' borderRadius='10' marginTop='2' p='3'>
-                <Image source={{ uri: item?.pic }} style={styles.image} alt='image' />
-                <VStack space='2' pr='1' flex={1} m='1' >
-                    <Text style={[styles.text, { height: 30 }]}>{item?.name}</Text>
-                    <Text style={[styles.text, { height: 30 }]}>{item?.price} ريال</Text>
-                </VStack>
-                <VStack space='3' paddingLeft='2'  >
-                    <TouchableOpacity onPress={() => onLogOutPressHandler()}>
-                        <Icon name='trash-o' size={30} color={Colors.ERROR} />
-                    </TouchableOpacity>
-                    <HStack width='100' justifyContent='space-between'>
-                        <CustomButton title='-' onPress={() => setCart(cartMinus(cart))} buttonStyle={{ width: 29, height: 35, backgroundColor: Colors.PRIMARY_LIGHT }} textStyle={{ fontSize: 20, color: Colors.SECONDARY_LIGHT }} />
-                        <Text style={styles.text}>{cart}</Text>
-                        <CustomButton title='+' onPress={() => setCart(cartPlus(cart))} buttonStyle={{ width: 29, height: 35, backgroundColor: Colors.PRIMARY_LIGHT }} textStyle={{ fontSize: 20, color: Colors.SECONDARY_LIGHT }} />
-                    </HStack>
-                </VStack>
-                <QuestionModal
-                    option1="انصراف"
-                    option2="حذف"
-                    visible={logoutModalVisible}
-                    onClose={onCloseLogoutModal}
-                    option1OnPress={onCloseLogoutModal}
-                    option2OnPress={deleteCart}
-                    title="آیا مطمئنید که میخواهید حذف کنید؟"
-                />
-            </HStack>
-
-        </View>
+            </View>
+        </CustomContainer>
     )
 }
 
