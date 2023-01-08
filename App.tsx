d4f8302f1a14085741d811b4c4fa8155bf4465f7
Arman <arman_m121@yahoo.com>
2023-01-08 19:46:31 +0330

    add CustomContainer

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useNetInfo } from '@react-native-community/netinfo';
 import { NavigationContainer } from '@react-navigation/native';
 import {
   QueryClient,
@@ -6,36 +7,35 @@ import {
 import { NativeBaseProvider } from 'native-base';
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
+import { AppOfflineNotification } from '~/component';
 import HomeStack from '~/navigation/MainStack';
 import { navigationRef } from '~/navigation/Methods';
 import { Colors } from '~/style';
-import { CustomContainer, AppOfflineNotification } from '~/component';
-import { useNetInfo } from '@react-native-community/netinfo';
 const queryClient = new QueryClient()
 
 export default function App() {
+
   const { isConnected } = useNetInfo();
 
   if (isConnected === false) {
     return <AppOfflineNotification />;
   }
 
-
   return (
-    <CustomContainer>
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar barStyle="dark-content" hidden={false} translucent={false} backgroundColor={Colors.PRIMARY_LIGHT} />
-        <QueryClientProvider client={queryClient}>
-          <NativeBaseProvider >
-            <NavigationContainer ref={navigationRef}>
 
-              <HomeStack />
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="light-content" hidden={false} translucent={false} backgroundColor={Colors.PRIMARY_LIGHT} />
+      <QueryClientProvider client={queryClient}>
+        <NativeBaseProvider >
+          <NavigationContainer ref={navigationRef}>
 
-            </NavigationContainer>
-          </NativeBaseProvider>
-        </QueryClientProvider>
-      </SafeAreaView>
-    </CustomContainer>
+            <HomeStack />
+
+          </NavigationContainer>
+        </NativeBaseProvider>
+      </QueryClientProvider>
+    </SafeAreaView>
+
   );
 }
 
