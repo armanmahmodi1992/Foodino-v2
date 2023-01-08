a9f05b39701d63ec803db12294d85147aff457f7
Arman <arman_m121@yahoo.com>
2023-01-08 19:46:51 +0330

    change splash timer

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -9,7 +9,7 @@ export default function SplashScreen() {
     useEffect(() => {
         const timer = setTimeout(() => {
             navigate('TabNavigator')
-        }, 2000);
+        }, 1000);
     }, []);
 
     return (
