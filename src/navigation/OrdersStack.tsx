e2568871b1f9290f02da0b7b731a6e26afd19bc0
Arman <arman_m121@yahoo.com>
2023-01-14 15:22:28 +0330

    add new screen

--- src/navigation/OrdersStack.tsx
+++ src/navigation/OrdersStack.tsx
@@ -1,29 +1,75 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { OrdersScreen } from '~/screens';
-import { CustomHeader } from '~/component';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { DeliveredScreen, PostedScreen, PreparingScreen } from '~/screens'
+import { View } from 'native-base';
+import { Dimensions } from 'react-native';
+import { Colors } from '~/style';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+export type TopTabNavigatorStackParamList = { TopTabNavigator: undefined };
+export const WIDTH = Dimensions.get('window').width / 3;
 
-export type OrdersStackParamList = { OrdersScreen: { data: any } };
+const Tab = createMaterialTopTabNavigator();
 
-const Stack = createNativeStackNavigator();
+const screens = [
+    {
+        name: 'DeliveredScreen',
+        component: DeliveredScreen,
+        options: { title: 'تحویل داده شده' }
+    },
+    {
+        name: 'PostedScreen',
+        component: PostedScreen,
+        options: { title: 'ارسال شده' }
+    },
+    {
+        name: 'PreparingScreen',
+        component: PreparingScreen,
+        options: { title: 'در حال آماده سازی' }
+    }
+];
 
-export default function OrdersStackNavigator() {
+export default function TopTabNavigator() {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-                name={'OrdersScreen'}
-                component={OrdersScreen}
-                options={
-                    {
-                        headerTitle: ' سفارشات',
-                        headerShown: true,
-                        header: ({ route, options, navigation }: any) => (
-                            <CustomHeader back {...{ route, options, navigation }} />
-                        ),
-                    }
-                }
-            />
+        <View flex='1' >
+            <Tab.Navigator
+                initialRouteName='PreparingScreen'
+                backBehavior='initialRoute'
+                screenOptions={({ route }) => ({
+                    tabBarIndicatorStyle: {
+                        backgroundColor: Colors.GARY_5
+                    },
 
-        </Stack.Navigator>
+                    tabBarLabelStyle: {
+                        fontWeight: 'bold',
+                        fontSize: 15
+                    },
+                    tabBarItemStyle: {
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        alignSelf: 'center'
+                    },
+                    tabBarIcon: ({ focused, color }) => {
+                        var iconName: any;
+                        if (route.name === 'PreparingScreen') {
+                            iconName = focused ? 'local-dining' : 'local-dining';
+                        }
+                        else if (route.name === 'PostedScreen') {
+                            iconName = focused ? 'delivery-dining' : 'delivery-dining';
+                        }
+                        else if (route.name === 'DeliveredScreen') {
+                            iconName = focused ? 'check' : 'check';
+                        }
+                        return <View ><Icon name={iconName} size={20} color={color} /></View>;
+                    },
+                    tabBarActiveTintColor: Colors.SECONDARY,
+                    tabBarInactiveTintColor: Colors.GARY_3,
+                })}>
+                {screens.map(screen => (
+                    //@ts-ignore
+                    <Tab.Screen key={screen.name} {...screen} />
+                ))}
+            </Tab.Navigator>
+        </View>
+
     );
 }
