ebdd00908114de1b768b899d1cce42d2cacf758f
Arman <arman_m121@yahoo.com>
2023-01-14 15:13:32 +0330

    change styling

--- src/component/atom/EmptyOrder.tsx
+++ src/component/atom/EmptyOrder.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
-import { image } from '~/utils';
+import { Colors } from '~/style';
+import { image, Style } from '~/utils';
 export default function EmptyOrder() {
     return (
         <View style={styles.container}>
-            <View style={styles.content}>
-                <Image source={{ uri: image.noOrder }} style={{ width: 200, height: 200 }} />
-                <Text style={styles.text}>شما سفارشی ثبت نکردید</Text>
-            </View>
+            <Image source={{ uri: image.noOrder }} style={{ width: 200, height: 200, marginBottom: 20 }} />
+            <Text style={styles.text}>شما سفارشی ثبت نکردید</Text>
         </View>
     )
 }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    },
-    content: {
-        flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        paddingTop: 200
+
     },
     text: {
         fontSize: 25,
-        marginTop: 10
+        marginTop: 10,
+        fontFamily: Style.fontFamily.bold,
+        color: Colors.GARY_1
     }
 
 });
