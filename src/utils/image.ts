dd0d2d32826c2d422ba7f828b60fb20f3900263d
Arman <arman_m121@yahoo.com>
2023-01-15 21:34:16 +0330

    change image url

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,4 +1,4 @@
 export const splash = 'https://i.postimg.cc/VvyxQdRw/splash.png';
 export const header = 'https://i.postimg.cc/c17CbZDw/header-food.jpg';
-export const noCart = 'https://i.postimg.cc/mDmLTZK5/shopping-cart.png';
-export const noOrder = 'https://i.postimg.cc/8z0yzMjF/empty-order.png';
+export const noCart = 'https://i.postimg.cc/CKMjfprg/empty-cart.png';
+export const noOrder = 'https://i.postimg.cc/VkdG0MJg/empty-order.png';
