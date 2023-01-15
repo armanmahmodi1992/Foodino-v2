2a67f18aca6402a8a61336958b7504ba791e79cb
Arman <arman_m121@yahoo.com>
2023-01-15 21:23:36 +0330

    redesign for rtl language

--- src/component/atom/SumCart.tsx
+++ src/component/atom/SumCart.tsx
@@ -9,16 +9,16 @@ import { navigate } from '~/navigation/Methods';
 export default function SumCart(props: { totalCount: number; totalPrice: number }) {
     const { totalCount, totalPrice } = props;
     return (
-        <VStack w='100%' h='170' alignItems='flex-end' space='6' p='2' borderWidth='2' borderColor={Colors.GARY_5}>
+        <VStack w='100%' h='170' alignItems='flex-end' space='6' px='4' borderWidth='1' borderColor={Colors.GARY_5}>
             <HStack justifyContent='space-between' w='100%' borderBottomWidth='2' borderColor={Colors.GARY_5} h='42' pb='3' >
-                <Text style={styles.text}>تعداد: {totalCount}</Text>
                 <Text style={styles.text}>خلاصه سبد</Text>
-            </HStack>
+                <Text style={styles.text}>تعداد: {totalCount}</Text>
+            </HStack >
             <HStack justifyContent='space-between' w='100%' alignItems='center'>
-                <CustomButton title='ادامه خرید' onPress={() => navigate('FurtherInformation')} buttonStyle={{ width: 130, height: 35, backgroundColor: Colors.SECONDARY_LIGHT }} textStyle={{ fontSize: 20, color: Colors.PRIMARY_LIGHT }} />
                 <Text style={styles.text}>جمع: {totalPrice} ريال</Text>
+                <CustomButton title='ادامه خرید' onPress={() => navigate('FurtherInformation')} buttonStyle={{ width: 130, height: 35, backgroundColor: Colors.SECONDARY_LIGHT }} textStyle={{ fontSize: 20, color: Colors.PRIMARY_LIGHT }} />
             </HStack>
-        </VStack>
+        </VStack >
     )
 }
 const styles = StyleSheet.create({
