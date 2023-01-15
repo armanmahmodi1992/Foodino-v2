216300f7d7bd3cfff1363babdda2132b3bc8e7d2
Arman <arman_m121@yahoo.com>
2023-01-15 21:31:37 +0330

    add QuestionModal for logout

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Image, TouchableOpacity, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Colors } from '~/style'
 import Icon from 'react-native-vector-icons/AntDesign'
 import { HStack, VStack } from 'native-base';
 import { authStore } from '~/store/AuthStore';
 import { navigate } from '~/navigation/Methods';
+import { QuestionModal } from '~/component';
 import { image, Style } from '~/utils'
 
 export default function UserScreen() {
@@ -12,12 +13,21 @@ export default function UserScreen() {
     const { setIsLogin } = authStore(state => state);
     const { setToken } = authStore(state => state);
     const { token } = authStore();
+    const [logoutModalVisible, setLogoutModalVisible] = useState<boolean>(false);
 
     let name, address, email
 
     if (token != '') {
         [{ name, address, email }] = token
     }
+
+    const onLogOutPressHandler = () => {
+        setLogoutModalVisible(true);
+    };
+
+    const onCloseLogoutModal = () => {
+        setLogoutModalVisible(false);
+    };
 
     const handleLogout = () => {
         setIsLogin(false);
@@ -29,20 +39,34 @@ export default function UserScreen() {
         <VStack flex={1} backgroundColor='white'>
             <Image source={{ uri: image.header }} style={styles.image} />
             <Image source={{ uri: image.splash }} style={styles.logo} />
-            <TouchableOpacity style={styles.setting} onPress={() => navigate('SettingScreen')}>
-                <Icon size={35} color={Colors.SECONDARY} name='setting' />
-            </TouchableOpacity>
-            <VStack mt='3' mr='2' space='4' alignItems='flex-end' borderBottomWidth='4' borderBottomColor={Colors.GARY_5} pb='5'>
-                <Text style={styles.text}>  نام : {name} </Text>
-                <Text style={styles.text}>   ایمیل : {email}</Text>
-                <Text style={styles.text}>  آدرس : {address}  </Text>
+            <HStack w='100%' position='absolute' mt='6px' justifyContent='space-between' px='2' alignItems='center'>
+                <TouchableOpacity onPress={() => navigate('HomeScreen')}>
+                    <Icon size={30} color={Colors.PRIMARY_LIGHT} name='right' />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigate('SettingScreen')}>
+                    <Icon size={30} color={Colors.PRIMARY_LIGHT} name='setting' />
+                </TouchableOpacity>
+            </HStack>
+            <VStack px='4' space='4' >
+                <Text style={styles.text}>  نام: {name} </Text>
+                <Text style={styles.text}>  ایمیل: {email}</Text>
+                <Text style={styles.text}>  آدرس: {address}  </Text>
             </VStack>
-            <HStack flexDirection='row-reverse' mt='8' p='4' space='4' alignItems='center'>
-                <TouchableOpacity onPress={handleLogout}>
-                    <Icon size={40} color={Colors.SECONDARY_LIGHT} name='logout' />
+            <HStack mt='8' px='4' space='4' alignItems='center'>
+                <TouchableOpacity onPress={() => onLogOutPressHandler()}>
+                    <Icon size={25} color={Colors.SECONDARY_LIGHT} name='logout' />
                 </TouchableOpacity>
                 <Text style={styles.logoutText}>خروج از حساب کاربری</Text>
             </HStack>
+            <QuestionModal
+                option1="انصراف"
+                option2="تایید"
+                visible={logoutModalVisible}
+                onClose={onCloseLogoutModal}
+                option1OnPress={onCloseLogoutModal}
+                option2OnPress={handleLogout}
+                title="برای خروج از حساب کاربری دکمه تایید را بزنید."
+            />
         </VStack >
     )
 }
@@ -56,8 +80,8 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: 250,
-        borderWidth: 1,
-        borderRadius: 10,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
         borderColor: Colors.GARY_5,
         backgroundColor: Colors.GARY_4
     },
@@ -71,22 +95,13 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         backgroundColor: Colors.PRIMARY_LIGHT,
     },
-    setting: {
-        position: 'absolute',
-        margin: 4,
-        marginLeft: 7,
-        width: 33,
-        height: 33,
-        borderRadius: 16
-    },
     text: {
         backgroundColor: Colors.PRIMARY_LIGHT,
         borderWidth: 1,
         borderColor: Colors.PRIMARY_LIGHT,
         borderRadius: 10,
-        width: '97%',
-        height: 35,
-        textAlign: 'right',
+        width: '100%',
+        height: 30,
         fontSize: 19,
         fontWeight: Style.fontWeight.bold,
         justifyContent: 'center'
