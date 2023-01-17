1d406156a1722357adb785ebab87852d0f368b51
Arman <arman_m121@yahoo.com>
2023-01-17 11:12:57 +0330

    add icon into SettingData

--- src/services/SettingData.ts
+++ src/services/SettingData.ts
@@ -1,8 +1,8 @@
 
 
-export const item = [{ id: 1, name: 'ویرایش اطلاعات',routeName: 'EditScreen' },
-{ id: 1, name: 'درباره فودینو', routeName: 'AboutApplicationScreen' },
-{ id: 2, name: 'روشهای پرداخت', routeName: 'PaymentMethodsScreen' },
-{ id: 3, name: ' شیوه ارسال سفارش', routeName: 'DeliveryScreen' },
-{ id: 4, name: ' پشتیبانی', routeName: 'SupportScreen' },
+export const item = [{ id: 1, name: 'ویرایش اطلاعات',routeName: 'EditScreen',icon:'mode-edit' },
+{ id: 1, name: 'درباره فودینو', routeName: 'AboutApplicationScreen',icon:'info-outline' },
+{ id: 2, name: 'روشهای پرداخت', routeName: 'PaymentMethodsScreen',icon:'payments' },
+{ id: 3, name: ' شیوه ارسال سفارش', routeName: 'DeliveryScreen',icon:'delivery-dining' },
+{ id: 4, name: ' پشتیبانی', routeName: 'SupportScreen',icon:'support-agent' },
 ]
