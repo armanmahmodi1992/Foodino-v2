45decd77c608acd72ca088d1a2f433e218a58d50
Arman <arman_m121@yahoo.com>
2023-02-06 11:42:32 +0330

    add StatusBar

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -2,20 +2,29 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SplashScreen } from '~/screens';
 import { TabNavigator } from '~/navigation';
+import { StatusBar } from 'native-base';
+import { useTheme } from '@react-navigation/native';
+import themStore from '~/store/ThemeStore';
 export type MainStackParamList = { SplashScreen: undefined; TabNavigator: undefined };
 export default function MainStackNavigator() {
+    const { theme } = themStore();
+    const { colors } = useTheme();
     const Stack = createNativeStackNavigator();
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-                name={'SplashScreen'}
-                component={SplashScreen}
-            />
-            <Stack.Screen
-                name={'TabNavigator'}
-                component={TabNavigator}
-            />
+        <>
+            <StatusBar barStyle={theme ? "light-content" : "dark-content"} backgroundColor={theme ? colors.PRIMARY_DARK : colors.PRIMARY_LIGHT} />
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
 
-        </Stack.Navigator>
+                <Stack.Screen
+                    name={'SplashScreen'}
+                    component={SplashScreen}
+                />
+                <Stack.Screen
+                    name={'TabNavigator'}
+                    component={TabNavigator}
+                />
+
+            </Stack.Navigator>
+        </>
     );
 }
